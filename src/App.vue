c8e9296e5a6fed2450a3672acde814eab9f9cd8a
Rohit Gupta <rohitgupta.cct@gmail.com>
2020-03-16 17:51:52 +0530

    search component and youtube api

---
+++ src/App.vue
@@ -0,0 +1,30 @@
+<template>
+    <div>
+        <SearchBar @termChange = "onTermChange"> </SearchBar>
+    </div>
+</template>
+ 
+
+<script>
+import axios from 'axios';
+import SearchBar from './components/SearchBar';
+const API_KEY = 'AIzaSyBYMTvfZ0H5iuxMDFhWz8G4ErpX2n6Hx0s'
+export default {
+    name: 'App' ,  
+    components:{
+        SearchBar,
+    },
+    methods:{
+        onTermChange(searchTerm){
+            axios.get('https://www.googleapis.com/youtube/v3/search',{
+                params:{
+                    key:API_KEY,
+                    type:'video',
+                    part:'snippet',
+                    q:searchTerm
+                }
+            }).then(response => console.log(response));
+        }
+    }
+}; 
+</script>
